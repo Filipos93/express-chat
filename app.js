26d366a4cee04ddd73b107481c5692cefdd617b3
Filip M <filipm93@gmail.com>
2017-06-14 23:21:34 +0200

    getting 10 last messages from redis, enhanced user experience

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var io = require('socket.io');
+var redisClient = require('redis').createClient();
 
 // var server = require('http').createServer(app);
 // var io = require('socket.io')(server);
@@ -9,10 +10,29 @@ var io = require('socket.io');
 var server  = app.listen(3000, function(){console.log('listening on port 3000')});
 var io      = require('socket.io').listen(server);
 
+var storeMessages = function(name, data){
+	redisClient.lpush("messages", JSON.stringify({author: name, msg: data}), function(err, res){
+		if(err){
+			throw err;
+		}
+		console.log('messages stored in redis: ', res);
+	});
+	redisClient.ltrim("messages", 0, 9);
+}
+
 io.on('connection', function(client){
 	console.log('client connected');
 	client.emit('message', {author: 'server', msg: 'Connected to chat'});
-
+	
+	redisClient.lrange("messages", 0, -1, function(err, messages){
+		// console.log(messages);
+		messages = messages.reverse();
+		messages.forEach(function(message){
+			client.emit('message', message);
+			console.log(message);
+		});	
+	});
+	
 	client.on('join', function(data){
 		client.nickname = data;
 	});
@@ -24,6 +44,7 @@ io.on('connection', function(client){
 		message.author = client.nickname;
 		client.broadcast.emit('message', message);
 		client.emit('message', message);
+		storeMessages(client.nickname, data.msg);
 	});
 });
 
--- public/js/client.js
+++ public/js/client.js
@@ -1,14 +1,19 @@
 'use strict';
 $(function(){
 
+	$('#chat-window').children('div').remove();
+
 	let socket = io.connect('http://localhost:3000');
 
 	let insertMessage = function(message){
+		if(typeof message != 'object'){
+			message = JSON.parse(message);
+		};
 		let content = message.msg;
 		let author = message.author;
 
-		let $message = $(`<div class="alert alert-success"> <p class="author">${author}:</p> <span>${content}</span> </div>`);
-		$('.chat-window').append($message);
+		let messageToAppend = $(`<div class="alert alert-success"> <p class="author">${author}:</p> <span>${content}</span> </div>`);
+		messageToAppend.hide().appendTo('.chat-window').fadeIn(300);
 	}
 
 	socket.on('connect', function(data){
@@ -19,16 +24,19 @@ $(function(){
 
 	socket.on('message', function(data){
 		insertMessage(data);
+		let chatWindow = $('.chat-window');
+		let windowHeight = chatWindow[0].scrollHeight;
+		chatWindow.scrollTop(windowHeight);
 	});
 
 	$('#chat-form').on('submit', function(e){
 		e.preventDefault();
-		var message = {};
-		message.msg = $('#chat-input').val();
-		// message.author = 'you';
-		// insertMessage(message);
+		let message = {};
+		let chatInput = $('#chat-input');
+		message.msg = chatInput.val();
 		socket.emit('message', message);
 		this.reset();
+		chatInput.focus();
 	});
 
 })
